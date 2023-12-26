3b001693f4af84309f44d95fa90af942445552ce
ComeAlongErica <ericaraephoto@gmail.com>
2023-12-26 16:18:21 -0600

    some canvas stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form/Form';
+import Canvas from './components/Canvas/Canvas';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Form />
+        <Canvas img={require('./ella.JPG')} height={350} width={500} />
       </header>
     </div>
   );
--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,12 +1,35 @@
-import React, { FC } from 'react';
+import React, { FC, createRef } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Canvas.module.css';
 
-interface CanvasProps {}
+interface CanvasProps {
+  img: string,
+  height: number;
+  width: number;
+}
 
-const Canvas: FC<CanvasProps> = () => (
-  <div className={styles.Canvas}>
-    Canvas Component
-  </div>
-);
-
+const Canvas: FC<CanvasProps> = ({ img, height, width }) => {
+  const canvas = createRef<HTMLCanvasElement>();
+  React.useEffect(() => {
+    debugger
+    const context = canvas?.current?.getContext('2d');
+    if (!context || img.trim() === '') {
+        return;
+    }
+    const image = new Image();
+    image.src = img;
+    image.onload = () => {
+    context.drawImage(image, 0, 0, canvas.current!.width, canvas.current!.height);
+  };
+  }, [canvas, img]);
+return (
+    <canvas ref={canvas} height={height} width={width} />
+  );
+}
 export default Canvas;
+
+Canvas.propTypes = {
+  img: PropTypes.string.isRequired,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+};
